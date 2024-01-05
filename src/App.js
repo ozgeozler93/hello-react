7d86b4f7815ad268f967ebe008e0c31fabb50404
Ozge <ozgeozler_93@hotmail.com>
2024-01-05 19:19:21 +0300

    First modification for home page.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Ozgee
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React Ozgee
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div>
+      <h1>My React App</h1>
+      <p>Welcome to my React App, more to come...</p>
     </div>
   );
 }
